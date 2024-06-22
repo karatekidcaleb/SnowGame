26a04ec28a389e59e518ac3370b538ccd510e1da
Caleb Leeb <karatekidcaleb@hotmail.com>
2024-06-22 16:41:29 -0400

    Working collision detection implemented, beginning of plant class, correct scaling of all sprites.

--- SnowGame/Level.cs
+++ SnowGame/Level.cs
@@ -7,12 +7,13 @@ public partial class Level : Node2D
 	public override void _Ready()
 	{
 		var tree_spawner = GD.Load<PackedScene>("res://Treething.tscn");
-		for (int i=0; i< new Random().Next()%5; i++)
+		for (int i=0; i< new Random().Next()%3 + 2.0f; i++)
 		{
 			AnimatedSprite2D instance = (AnimatedSprite2D)tree_spawner.Instantiate();
+			float random_size = GD.Randf() * 0.5f + 0.2f;
 			instance.Scale = new Vector2(
-				x: GD.Randf() * 3 + 1,
-				y: GD.Randf() * 3 + 1
+				x: random_size,
+				y: random_size
 			);
 			instance.Position = new Vector2(
 				x: GD.Randf() * GetViewportRect().Size.X,
